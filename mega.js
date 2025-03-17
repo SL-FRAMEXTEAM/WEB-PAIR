eb4dc3b1f4016a1b8e72a1612f887aca9d1f7a40
SL-FRAMEXTEAM <darkslframexteam@gmail.com>
2025-03-17 21:07:53 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "darkslframexteam@gmail.com",
+  password: "F1234509876web",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
